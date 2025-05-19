987c88fb0b4c895718d7ce21df27053343e133a1
Norwood91 <robnorwood91@gmail.com>
2025-05-19 14:46:29 -0700

    Added a test that tested the case insensitivity. The test passed. All of the testing is completed and all passed.

--- test_char_count.py
+++ test_char_count.py
@@ -3,24 +3,29 @@ from main import charCount
 
 class TestCharCount(unittest.TestCase):
     
-    # def test_basic_input(self):
-    #     result = charCount("Hello")
-    #     expected = {'h':1, 'e':1, 'l':2, 'o':1}
-    #     self.assertEqual(result, expected)
+    def test_basic_input(self):
+        result = charCount("Hello")
+        expected = {'h':1, 'e':1, 'l':2, 'o':1}
+        self.assertEqual(result, expected)
 
-    # def test_ignore_spaces_punc(self):
-    #     result = charCount("Hello, World!")
-    #     expected = {'h':1, 'e':1, 'l':3, 'o':2, 'w':1, 'r':1, 'd': 1}
-    #     self.assertEqual(result, expected)
+    def test_ignore_spaces_punc(self):
+        result = charCount("Hello, World!")
+        expected = {'h':1, 'e':1, 'l':3, 'o':2, 'w':1, 'r':1, 'd': 1}
+        self.assertEqual(result, expected)
 
-    # def test_numbers_and_letters(self):
-    #     result = charCount("abc123")
-    #     expected = {'a':1, 'b':1, 'c':1, '1':1, '2':1, '3':1}
-    #     self.assertEqual(result, expected)
+    def test_numbers_and_letters(self):
+        result = charCount("abc123")
+        expected = {'a':1, 'b':1, 'c':1, '1':1, '2':1, '3':1}
+        self.assertEqual(result, expected)
 
     def test_empty_string(self):
         result = charCount("")
         expected = {}
+        self.assertEqual(result, expected)
+    
+    def test_case_insensitivity(self):
+        result = charCount("AabB")
+        expected = {'a':2, 'b':2}
         self.assertEqual(result, expected)
 
 if __name__ == "__main__":
