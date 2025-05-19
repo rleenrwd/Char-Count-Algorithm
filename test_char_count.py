11758a67b3fbedd64d262b5fbe7a9bedaea53775
Norwood91 <robnorwood91@gmail.com>
2025-05-19 14:35:04 -0700

    Added a successful test for testing basic input functionality.

--- test_char_count.py
+++ test_char_count.py
@@ -2,4 +2,14 @@ import unittest
 from main import charCount
 
 class TestCharCount(unittest.TestCase):
-    pass
+    
+    def test_basic_input(self):
+        result = charCount("Hello")
+        expected = {'h':1, 'e':1, 'l':2, 'o':1}
+        self.assertEqual(result, expected)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
